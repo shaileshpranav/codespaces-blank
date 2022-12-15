180acbe206ff22c2017cf97d444cf9b83ff784ae
Shailesh Pranav Rajendran <shaileshpranav.r@gmail.com>
2022-12-15 06:59:29 -0500

    [Task 2.8.4] Added tests

--- test/navigation_test.cpp
+++ test/navigation_test.cpp
@@ -11,12 +11,13 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <cv_bridge/cv_bridge.h>
 
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h>
 
-#include "navigation/navigation.hpp"
+#include "../include/wall-e/navigation.hpp"
 
 // Global variables
 bool message_received;
@@ -42,23 +43,38 @@ void pose_callback(const geometry_msgs::PoseStampedConstPtr& msg) {
     pose_message.pose = msg->pose;
 }
 
-TEST(test_navigation_class, test_bin_goal_setting) {
-    // Arrange
-    message_received = false;
+TEST(test_navigation_class, test_get_object_pose) {
+
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("/move_base_simple/goal", 10,
-                                                            pose_callback);
     geometry_msgs::PoseStamped expected_msg;
-    expected_msg.pose.orientation.w = 1;
-    expected_msg.header.frame_id = "map";
+    expected_msg.pose.orientation.w = 0;
+    expected_msg.header.frame_id = "";
 
     // Act
     Navigation navigator(&nh);
-    navigator.set_bin_location_as_goal();
+    navigator.get_object_pose("map");
 
     // Assert
-    ASSERT_TRUE(wait_for_message(message_received, 3));
+    // ASSERT_TRUE(wait_for_message(message_received, 3));
     EXPECT_EQ(pose_message.pose.orientation, expected_msg.pose.orientation);
     EXPECT_EQ(pose_message.pose.position, expected_msg.pose.position);
     EXPECT_EQ(pose_message.header.frame_id, expected_msg.header.frame_id);
 }
+std::array<XmlRpc::XmlRpcValue, 5> goal_pos =   {(-1.752882, 3.246192),(6.5, 2.0),(-0.289296, -1.282680),(7.710214, -1.716889)};
+
+// TEST(test_navigation_class, test_set_room_pos){
+//     ros::NodeHandle nh;
+//     Navigation navi(&nh);
+//     bool flag{true};
+//     navi.set_room_pos(goal_pos);
+//     for(int i = 0;i<4;i++)
+//     {
+//         if(!navi.explorer_goal[i].target_pose.pose.position.x == double(goal_pos[i][0]) || !navi.explorer_goal[i].target_pose.pose.position.y == double(goal_pos[i][1]))
+//             flag = false; 
+//     }
+//     EXPECT_TRUE(flag);
+// }
+
+// TEST(test_navigation_class, test_robot_pose_cb){
+//     EXPECT_EQ(navi.)
+// }
